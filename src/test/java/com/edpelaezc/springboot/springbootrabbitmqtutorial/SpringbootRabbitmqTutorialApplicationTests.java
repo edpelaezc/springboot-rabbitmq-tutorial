ab686ae3dec335fa7701e88e57e7978303155282
Eduardo Peláez <eapelaezc@correo.url.edu.gt>
2024-05-10 21:26:30 -0600

    feat: Initial commit

---
+++ src/test/java/com/edpelaezc/springboot/springbootrabbitmqtutorial/SpringbootRabbitmqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edpelaezc.springboot.springbootrabbitmqtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
